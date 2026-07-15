ba0d96834b74c22bc0960d32abdc88a150ac724c
uwscct <uwscreativecomptech@gmail.com>
2026-07-15 16:54:19 +0100

    Update VRThumbstickManager.js

--- live/js/vr-engine/VRThumbstickManager.js
+++ live/js/vr-engine/VRThumbstickManager.js
@@ -1,3 +1,25 @@
+/*
+* BRANI-360: An Agnostic Browser-Based Research Sandbox Architecture for AI Audio Generation on Networks of 360° Images
+* Copyright (C) 2026 Dr Marco Gilardi, University of the West of Scotland.
+* * This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* * This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+* * You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+* * -------------------------------------------------------------------------
+* COMMERCIAL LICENSING
+* BRANI-360 is dual-licensed. The above AGPLv3 license applies to open-source 
+* and academic research use. If you wish to integrate this software into a 
+* closed-source or commercial application, you must obtain a proprietary 
+* commercial license. 
+* * Please contact Marco.Gilardi@uws.ac.uk for commercial licensing details.
+* -------------------------------------------------------------------------
+*/
 export class VRThumbstickManager {
     static register() {
         if (typeof AFRAME === 'undefined' || AFRAME.components['contextual-thumbsticks']) return;
